bf833f72eba7052670d6e5eeb88c21cc12a48522
Andreas Kunzendorf <andreaskunzendorf@gmail.com>
2024-02-04 20:41:10 +0100

    Fix git keymaps

--- after/plugin/keymaps.lua
+++ after/plugin/keymaps.lua
@@ -3,6 +3,6 @@ vim.keymap.set('n', '<leader>sr', require('telescope.builtin').oldfiles, { desc 
 
 -- Fugitive
 vim.keymap.set('n', '<leader>gg', '<cmd>Git<cr>', { desc = '[G]it' })
-vim.keymap.set('n', '<leader>gg', '<cmd>Git push<cr>', { desc = '[G]it [P]ush' })
-vim.keymap.set('n', '<leader>gg', '<cmd>Git pull<cr>', { desc = '[G]it pul[L]' })
+vim.keymap.set('n', '<leader>gp', '<cmd>Git push<cr>', { desc = '[G]it [P]ush' })
+vim.keymap.set('n', '<leader>gl', '<cmd>Git pull<cr>', { desc = '[G]it pul[L]' })
 
