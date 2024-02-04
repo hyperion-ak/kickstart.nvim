c9ee63f3ffb3a5f1f32b27283d653f5913925892
Andreas Kunzendorf <andreaskunzendorf@gmail.com>
2024-02-04 20:20:11 +0100

    Add git keymaps

--- after/plugin/keymaps.lua
+++ after/plugin/keymaps.lua
@@ -2,6 +2,7 @@
 vim.keymap.set('n', '<leader>sr', require('telescope.builtin').oldfiles, { desc = '[S]earch [R]ecently opened files' })
 
 -- Fugitive
-vim.keymap.set('n', '<leader>gg', '<cmd>Git<cr>', { desc = 'Open [G]it' })
-
+vim.keymap.set('n', '<leader>gg', '<cmd>Git<cr>', { desc = '[G]it' })
+vim.keymap.set('n', '<leader>gg', '<cmd>Git push<cr>', { desc = '[G]it [P]ush' })
+vim.keymap.set('n', '<leader>gg', '<cmd>Git pull<cr>', { desc = '[G]it pul[L]' })
 
