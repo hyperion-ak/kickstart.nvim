480f95d4537ad877a88c7a0584992d9c8927d49d
Andreas Kunzendorf <andreaskunzendorf@gmail.com>
2024-02-17 21:24:33 +0100

    Reformat lua code

--- after/plugin/keymaps.lua
+++ after/plugin/keymaps.lua
@@ -5,4 +5,3 @@ vim.keymap.set('n', '<leader>sr', require('telescope.builtin').oldfiles, { desc 
 vim.keymap.set('n', '<leader>gg', '<cmd>Git<cr>', { desc = '[G]it' })
 vim.keymap.set('n', '<leader>gp', '<cmd>Git push<cr>', { desc = '[G]it [P]ush' })
 vim.keymap.set('n', '<leader>gl', '<cmd>Git pull<cr>', { desc = '[G]it pul[L]' })
-
