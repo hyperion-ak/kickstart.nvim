1eb4f588caa2fc8ebd69212fb16a00b94886f0d2
Andreas Kunzendorf <andreaskunzendorf@gmail.com>
2024-02-17 21:05:46 +0100

    Disable netrw from loading

--- init.lua
+++ init.lua
@@ -44,6 +44,16 @@ P.S. You can delete this when you're done too. It's your config now :)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+
+-- Disable netrw plugin
+vim.g.loaded_netrw =  1
+vim.g.loaded_netrwPlugin =  1
+vim.g.netrw_banner =  0
+vim.g.netrw_liststyle =  3
+vim.g.netrw_browse_split =  4
+vim.g.netrw_altv =  1
+vim.g.netrw_winsize =  25
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    https://github.com/folke/lazy.nvim
 --    `:help lazy.nvim.txt` for more info
