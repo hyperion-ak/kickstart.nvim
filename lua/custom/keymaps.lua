df8dd75fdce652aee342ee4c944b34163057d3dd
Andreas Kunzendorf <andreaskunzendorf@gmail.com>
2024-03-16 19:09:28 +0100

    Move from fugitive to neogit

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -1,7 +1,2 @@
 -- Telescope
 vim.keymap.set('n', '<leader>sr', require('telescope.builtin').oldfiles, { desc = '[S]earch [R]ecently opened files' })
-
--- Fugitive
-vim.keymap.set('n', '<leader>gg', '<cmd>Git<cr>', { desc = '[G]it' })
-vim.keymap.set('n', '<leader>gp', '<cmd>Git push<cr>', { desc = '[G]it [P]ush' })
-vim.keymap.set('n', '<leader>gl', '<cmd>Git pull<cr>', { desc = '[G]it pul[L]' })
