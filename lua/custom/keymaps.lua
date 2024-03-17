61f2344ab0a61b92c5cdf01624eb4f8154548b3c
Andreas Kunzendorf <andreaskunzendorf@gmail.com>
2024-03-17 08:48:15 +0100

    Collect keymaps in one file

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -1,2 +1,9 @@
 -- Telescope
-vim.keymap.set('n', '<leader>sr', require('telescope.builtin').oldfiles, { desc = '[S]earch [R]ecently opened files' })
+vim.api.nvim_set_keymap('n', '<leader>sr', require('telescope.builtin').oldfiles, { desc = '[S]earch [R]ecently opened files' })
+
+-- git related
+vim.api.nvim_set_keymap('n', '<leader>gs', '<cmd>Neogit<CR>', { silent = true, noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>gc', '<cmd>Neogit commit<CR>', { silent = true, noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>gl', '<cmd>Neogit pull<CR>', { silent = true, noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>gp', '<cmd>Neogit push<CR>', { silent = true, noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>gB', '<cmd>Git blame<CR>', { silent = true, noremap = true })
