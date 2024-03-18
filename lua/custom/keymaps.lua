52eb64a18c3fee227b5a9342ffb929bbd7e8cffa
Andreas Kunzendorf <andreaskunzendorf@gmail.com>
2024-03-18 19:18:54 +0100

    Fix keymaps

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -1,9 +1,9 @@
 -- Telescope
-vim.api.nvim_set_keymap('n', '<leader>sr', require('telescope.builtin').oldfiles, { desc = '[S]earch [R]ecently opened files' })
+vim.keymap.set('n', '<leader>sr', require('telescope.builtin').oldfiles, { desc = '[S]earch [R]ecently opened files' })
 
 -- git related
-vim.api.nvim_set_keymap('n', '<leader>gs', '<cmd>Neogit<CR>', { silent = true, noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>gc', '<cmd>Neogit commit<CR>', { silent = true, noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>gl', '<cmd>Neogit pull<CR>', { silent = true, noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>gp', '<cmd>Neogit push<CR>', { silent = true, noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>gB', '<cmd>Git blame<CR>', { silent = true, noremap = true })
+vim.keymap.set('n', '<leader>gs', '<cmd>Neogit<CR>', { silent = true, noremap = true })
+vim.keymap.set('n', '<leader>gc', '<cmd>Neogit commit<CR>', { silent = true, noremap = true })
+vim.keymap.set('n', '<leader>gl', '<cmd>Neogit pull<CR>', { silent = true, noremap = true })
+vim.keymap.set('n', '<leader>gp', '<cmd>Neogit push<CR>', { silent = true, noremap = true })
+vim.keymap.set('n', '<leader>gB', '<cmd>Git blame<CR>', { silent = true, noremap = true })
